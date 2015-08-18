99c2ec40ed58e626a437b4854bc8e4ef78541ad2
Jamie <jamie.devine@doublespark.co.uk>
2015-08-18 15:48:43 +0100

    Add bundle file

---
+++ src/DoublesparkResponsiveImagesBundle.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of Contao.
+ */
+namespace Doublespark\ResponsiveImagesBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Configures the responsive images bundle
+ *
+ * @author Jamie Devine
+ */
+class DoublesparkResponsiveImagesBundle extends Bundle
+{
+}
