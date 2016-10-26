0f5849ad5daf1687753793e9027592dae2c8282c
Jamie <jamie.devine@doublespark.co.uk>
2016-10-26 16:45:23 +0100

    Make sure that JS breakpoints are always available

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -34,6 +34,25 @@ define('RESPONSIVE_IMAGES_VERSION', '1.1.10');
 if(TL_MODE == 'FE')
 {
 	$GLOBALS['TL_JAVASCRIPT']['responsive_images'] = 'web/bundles/doublesparkresponsiveimages/js/responsive-images.js|static';
+
+	// Default breakpoints
+	$arrBreakPoints = array(
+		'tablet' => 700,
+		'desktop' => 980,
+		'large' => 1400
+	);
+
+	// Override breakpoints if they have been set in the config
+	foreach($arrBreakPoints as $k => $v)
+	{
+		if(isset($GLOBALS['TL_CONFIG']['RESPONSIVE_IMAGES'][$k]) AND $GLOBALS['TL_CONFIG']['RESPONSIVE_IMAGES'][$k] != '')
+		{
+			$arrBreakPoints[$k] = $GLOBALS['TL_CONFIG']['RESPONSIVE_IMAGES'][$k];
+		}
+	}
+
+	// Add JS breakpoints to head - used by the JS script
+	$GLOBALS['TL_HEAD']['responsiveImageBreakpoints'] = '<script type="text/javascript">window.responsiveBreakPoints={"tablet":'.$arrBreakPoints['tablet'].',"desktop":'.$arrBreakPoints['desktop'].',"large":'.$arrBreakPoints['large']."};</script>";
 }
 
 // Add responsive image element
--- src/Resources/contao/elements/ContentResponsiveImage.php
+++ src/Resources/contao/elements/ContentResponsiveImage.php
@@ -73,24 +73,6 @@ class ContentResponsiveImage extends \ContentElement
 	 */
 	protected function compile()
 	{
-		// Default breakpoints
-		$arrBreakPoints = array(
-			'tablet' => 700,
-			'desktop' => 980,
-			'large' => 1400
-		);
-
-		// Override breakpoints if they have been set in the config
-		foreach($arrBreakPoints as $k => $v)
-		{
-			if(isset($GLOBALS['TL_CONFIG']['RESPONSIVE_IMAGES'][$k]) AND $GLOBALS['TL_CONFIG']['RESPONSIVE_IMAGES'][$k] != '')
-			{
-				$arrBreakPoints[$k] = $GLOBALS['TL_CONFIG']['RESPONSIVE_IMAGES'][$k];
-			}
-		}
-
-		// Add JS breakpoints to head - used by the JS script
-		$GLOBALS['TL_HEAD']['responsiveImageBreakpoints'] = '<script type="text/javascript">window.responsiveBreakPoints={"tablet":'.$arrBreakPoints['tablet'].',"desktop":'.$arrBreakPoints['desktop'].',"large":'.$arrBreakPoints['large']."};</script>";
 
 		$this->addImageToTemplate($this->Template, $this->arrData);
 
